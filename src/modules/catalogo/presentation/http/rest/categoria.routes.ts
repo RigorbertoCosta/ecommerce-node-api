7491a6f73e12cdab0257a5d478dcb80616983874
Rigorberto B. Costa <rigobertobatistawow@gmail.com>
2023-11-30 21:25:49 -0300

    atualizar-deletar-aula-04/12

--- src/modules/catalogo/presentation/http/rest/categoria.routes.ts
+++ src/modules/catalogo/presentation/http/rest/categoria.routes.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-import { inserirCategoriaController, recuperarCategoriaPorIdController, recuperarTodasCategoriasController } from './controllers';
+import express, { response } from 'express';
+import { atualizarCategoriaExpressController, deletarCategoriaExpressController, inserirCategoriaController, recuperarCategoriaPorIdController, recuperarTodasCategoriasController } from './controllers';
+import { request } from 'http';
 
 const categoriaRouter = express.Router();
 
@@ -17,5 +18,15 @@ categoriaRouter.post(
     '/',
     (request, response, next) =>  inserirCategoriaController.inserir(request, response, next)
 );
+
+categoriaRouter.put(
+    '/:id',
+    (request, response, next) => atualizarCategoriaExpressController.atualizar(request, response, next)
+)
+
+categoriaRouter.delete(
+    '/:id',
+    (request, response, next) => deletarCategoriaExpressController.deletar(request, response, next)
+)
 
 export { categoriaRouter };
